46a6ea1a89aa3f87bb115f2ee89b2482aa1baf9f
HumbledImran <90126902+HumbledImran@users.noreply.github.com>
2023-09-02 08:12:49 +0530

    Create sep2_helloWorld.py

---
+++ sep2_helloWorld.py
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/523b4ff7adca849afe000035/solutions/python
+
+# Write a function `greet` that returns "hello world!"
+def greet():
+    return 'hello world!'
+
+greet()
