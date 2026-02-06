c2d5b10b4500a6948949ba6205d3134a934bb8e1
Imran Shaik <90126902+imranshahshaik@users.noreply.github.com>
2026-02-06 10:41:26 +0530

    Time: 9 ms (38.09%), Space: 63.6 MB (67.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str = ''+x
+    for(let i = 0; i<Math.floor(str.length)/2; i++){
+        if(str[i]!==str[str.length-1-i]) return false
+        
+    }
+    return true;
+};
